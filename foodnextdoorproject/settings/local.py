5a3db6bb07f90b7f1c6b7bd258a38775204d3002
Arpith Muddi <arpith.muddi@sap.com>
2021-08-07 09:56:04 +0200

    Split dev/testing/production environments

--- foodnextdoorproject/settings/local.py
+++ foodnextdoorproject/settings/local.py
@@ -1,7 +1,12 @@
 from base import *
+import os
+from dotenv import load_dotenv
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# load the environment variables from .env files
+load_dotenv()
 
 DATABASES = {
     'default': {
