6c105a8f24e657455a32c3f923b5747c686097c7
Arpith Muddi <arpith.muddi@sap.com>
2022-01-04 17:36:36 +0100

    dummy data

---
+++ food/foodCategories.py
@@ -0,0 +1,686 @@
+dishCategory = [
+    {
+        "dishname": "Hara Bhara Salad",
+        "category": "Salads"
+    },
+    {
+        "dishname": "Kachumber Salad",
+        "category": "Salads"
+    },
+    {
+        "dishname": "Russian Salad",
+        "category": "Salads"
+    },
+    {
+        "dishname": "Bhutte Chatpate Salad",
+        "category": "Salads"
+    },
+    {
+        "dishname": "Rajwada Paneer Tikka",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Achari Aloo Tikka",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Cheese Chilli Paneer Tikka",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Jaisalmeri",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Bhatti ke Phal",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Rangeely Paneer",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Gobi Lal Mirch",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Tikka Makhmali",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Tandoori Shabnam",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Malai Dhingri",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Kali Mirch",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Aloo Tikka Amritsari",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paakashala Special Paneer Masala",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Mohini",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Kaju Mattar Paneer",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Methi Chaman",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Bhutta Mirch Da Saag",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Dil Pasand",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Mili Juli",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Peshawari",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Khaas",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Lehsuni Shabnam Palak",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Malai Kofta",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Cheese Palak Kofta",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Kali Mirch Masala",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Makai Mirchi Masala",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Lehsuni",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Aloo Amritsari",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Tikka Masaledar",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Shahi Darbar",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Keema Amritsari",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Hara Pyaaz",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Chole Amritsari",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Punjabi Paneer Bhurji",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Tawa",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Lakhnavi Methi Malai Kofta",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Mushroom Do Pyaaz",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Dal Makhani",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Dal Fry",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Dal Tadka",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Dal Palak",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Dal Methi",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Biryani",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Aloo Mattar ki Biryani",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Chole Paneer ki Biryani",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Hariyali Biryani",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Makkai Mushroom Biryani",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Kashmir ka Pulao",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Pulao",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Corn Methi Pulao",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Kaju Mattar Pulao",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Jeera Wale Chawal",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Dahi Chawal",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Saade Chawal",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Roti",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Naan",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Kulcha",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Missi Roti",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Lehsuni Naan",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Cheese Naan",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Chilli Milli Roti",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Methi Paratha",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Pudina Paratha",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Laccha Kulcha Roti",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Roti ki Tokri (Bread Basket)",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Boondi Raita",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Pineapple Raita",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Kheere ka Raita",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Mixed Raita",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Masala Papad",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Roasted Papad",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Crispy Herbs Potato",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Crispy Assorted Vegetables",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Crispy Corn Chilli Pepper",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Chilli Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Chilli Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Chilli Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Chilli Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Chilli Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Hong Kong Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Hong Kong Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Hong Kong Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Hong Kong Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Hong Kong Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower Manchurian",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball Manchurian",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato Manchurian",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese Manchurian",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom Manchurian",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Salt & Pepper Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Salt & Pepper Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Salt & Pepper Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Salt & Pepper Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Schezwan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Schezwan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Schezwan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Schezwan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Hunan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Hunan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Hunan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Hunan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Hunan Sauce",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese & Peas Fried Rice",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Vegetable Fried Rice",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Schezwan Fried Rice",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Combination Fried Rice",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Corn & Bell Pepper Fried Rice",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Hakka Noodles",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Schezwan Noodles",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Exotic Soft Noodles",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Shanghai Noodles",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Shahi Gajar Halwa",
+        "category": "Desserts"
+    },
+    {
+        "dishname": "Shahi Gulab Jamun",
+        "category": "Desserts"
+    },
+    {
+        "dishname": "Fruit Punch",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Virgin Mango Mary",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Jal Jeera",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Pudina Nimboo ki Shikanji",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Kairi Juice",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Buttermilk",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Lassi",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Mastaani Lassi",
+        "category": "Beverages"
+    },
+    {
+        "dishname": "Cauliflower in Chilli Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Chilli Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Chilli Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Chilli Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Chilli Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Hong Kong Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Hong Kong Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Hong Kong Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Hong Kong Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Hong Kong Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower Manchurian Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball Manchurian Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato Manchurian Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese Manchurian Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom Manchurian Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Salt & Pepper Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Salt & Pepper Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Salt & Pepper Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Salt & Pepper Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Schezwan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Schezwan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Schezwan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Schezwan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cauliflower in Hunan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Veg Ball in Hunan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Potato in Hunan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cottage Cheese in Hunan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Mushroom in Hunan Sauce Dry",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Kaju Masala",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Butter Masala",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Paneer Pahadi Tikka",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Stuffed Kulcha",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Stuffed Paratha",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Aloo Paratha",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Butter Naan",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Butter Kulcha",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Butter Roti",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Hot & Sour Soup",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Lemon Coriander Soup",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Manchow Soup",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Sweet Corn Soup",
+        "category": "Chinese"
+    },
+    {
+        "dishname": "Cream Of Veg Soup",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Cream Tomato Soup",
+        "category": "Indian"
+    },
+    {
+        "dishname": "Subzi Biryani + Buttermilk",
+        "category": "Beverage Combos"
+    },
+    {
+        "dishname": "Chole Paneer ki Biryani + Buttermilk",
+        "category": "Beverage Combos"
+    },
+    {
+        "dishname": "Subzi Hariyali Biryani + Buttermilk",
+        "category": "Beverage Combos"
+    }
+]
